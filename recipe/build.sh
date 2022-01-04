1bfdc61d660a83190570ea139884c55561ab9da8
Rémi Braun <67311115+remi-braun@users.noreply.github.com>
2022-01-04 15:10:39 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install methodtools
+pip install https://files.pythonhosted.org/packages/2b/13/973a50bb3802d4e915c291fc9b4df034dc91dc07b442cbb186eda7b117f4/methodtools-0.4.5.tar.gz
 pip install . --no-deps -vv
