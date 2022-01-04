20a9506ef3f28c664858d893abb86e897d9d62e7
Rémi Braun <67311115+remi-braun@users.noreply.github.com>
2022-01-04 15:15:11 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-pip install https://files.pythonhosted.org/packages/2b/13/973a50bb3802d4e915c291fc9b4df034dc91dc07b442cbb186eda7b117f4/methodtools-0.4.5.tar.gz
-pip install . --no-deps -vv
+pip install https://files.pythonhosted.org/packages/03/8c/91a6f66f359dfc2494a37bb20eac9dc811181b0ce16c83cf35ce9e6836df/eoreader-0.10.0-py3-none-any.whl --no-deps -vv
